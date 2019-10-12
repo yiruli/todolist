34d7cd7221efed73f2c5ae87916abdbcc3bc15da
yiruli <yiruli09@gmail.com>
2019-10-12 11:10:26 -0700

    Add files via upload

---
+++ precache-manifest.61b78142a1f6fa87998913cd6bc20341.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "411f008da6c4eb46aeb785043ca3e4ab",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "ebbe4d327fa8b20e4c90",
+    "url": "/todolist/static/css/main.cd2cf059.chunk.css"
+  },
+  {
+    "revision": "05067fe268b4ca03a666",
+    "url": "/todolist/static/js/2.6d7bba25.chunk.js"
+  },
+  {
+    "revision": "ebbe4d327fa8b20e4c90",
+    "url": "/todolist/static/js/main.f318e036.chunk.js"
+  },
+  {
+    "revision": "49f4a8bf9f5fb20ec2bc",
+    "url": "/todolist/static/js/runtime-main.07ae1fa1.js"
+  }
+]);
